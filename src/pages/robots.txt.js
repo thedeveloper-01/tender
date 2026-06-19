4d21db9778b98b4ff5c5458755221fe464a32d3a
Shubham Kumar <vr8120168439@gmail.com>
2026-06-19 13:49:16 +0530

    feat: show GEM bid doc fields on listing cards

--- src/pages/robots.txt.js
+++
@@ -1,9 +0,0 @@
-const siteUrl = 'https://cgtenders.com';
-export async function GET() {
-  const body = `User-agent: *
-Allow: /
-Disallow: /api/
-
-Sitemap: ${siteUrl}/sitemap.xml`;
-  return new Response(body, { headers: { 'Content-Type': 'text/plain' } });
-}
